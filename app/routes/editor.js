6dc3c5da9a375ce6a309d8686a3170b06071b56c
Pat O'Callaghan <pat@intercom.io>
2019-03-26 00:03:28 +0000

    Fixed some Prettier errors

--- app/routes/editor.js
+++ app/routes/editor.js
@@ -1,4 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class EditorRoute extends Route {
-}
+export default class EditorRoute extends Route {}
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,4 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class SignUpRoute extends Route {
-}
+export default class SignUpRoute extends Route {}
