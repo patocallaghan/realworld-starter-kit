1da07c44da3b34ad5ce73a9ccdd34648e55e29cc
Pat O'Callaghan <pat@intercom.io>
2019-04-01 09:36:11 +0100

    Add profile page

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,8 +1,22 @@
 import DS from 'ember-data';
-const { Model, attr } = DS;
+import { tracked } from '@glimmer/tracking';
+const { Model, attr, hasMany } = DS;
 
 export default class UserModel extends Model {
+  @tracked articles;
+
   @attr('string') bio;
   @attr('string') image;
   @attr('boolean') following;
+
+  @hasMany('article', { async: false, inverse: 'author' }) articles;
+
+  async loadArticles() {
+    let articles = await this.store.query('article', { author: this.id });
+    this.articles = articles;
+  }
+
+  fetchFavorites() {
+    return this.store.query('article', { favorited: this.id });
+  }
 }
