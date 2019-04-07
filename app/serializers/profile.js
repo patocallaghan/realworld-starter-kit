e3a3ae4c46a325e3fa9c3a99fdc17a09a3cd55dd
Pat O'Callaghan <pat@intercom.io>
2019-04-07 23:12:40 +0100

    Clean up profile normalize code

--- app/serializers/profile.js
+++ app/serializers/profile.js
@@ -4,14 +4,8 @@ export default class UserSerializer extends ApplicationSerializer {
   primaryKey = 'username';
 
   normalizeFindRecordResponse(store, primaryModelClass, payload) {
-    // TODO - Figure out why just mutating payload to be { profiles: { ... }} and calling `super` doesn't work
-    let profile = payload.profile;
-    return {
-      data: {
-        type: 'profile',
-        id: profile.username,
-        attributes: profile,
-      },
-    };
+    payload.profiles = payload.profile;
+    delete payload.profile;
+    return super.normalizeFindRecordResponse(...arguments);
   }
 }
