6c1227b683d1a411a56ccfecbf00bb8a3fd2f965
Pat O'Callaghan <pat@intercom.io>
2019-04-08 08:39:05 +0100

    Add homepage acceptance test

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,7 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
+    ENV.APP.apiHost = '';
   }
 
   // if (environment === 'production') {
